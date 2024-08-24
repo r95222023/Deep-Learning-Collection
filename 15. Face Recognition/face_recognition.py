e0842854e741b254c0e847028b40edc85799ad0f
Bo-Yan Huang <r95222023@gmail.com>
2024-08-24 15:32:40 +0800

    added 13 to 18

---
+++ 15. Face Recognition/face_recognition.py
@@ -0,0 +1,118 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import MaxPooling2D, AveragePooling2D
+from tensorflow.keras.layers import Concatenate
+from tensorflow.keras.layers import Lambda, Flatten, Dense
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import backend as K
+K.set_image_data_format('channels_last')
+import os
+import numpy as np
+import tensorflow as tf
+
+def triplet_loss(y_true, y_pred, alpha = 0.2):
+    """
+    Implementation of the triplet loss as defined by formula (3)
+    
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor images, of shape (None, 128)
+            positive -- the encodings for the positive images, of shape (None, 128)
+            negative -- the encodings for the negative images, of shape (None, 128)
+    
+    Returns:
+    loss -- real number, value of the loss
+    """
+    
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+
+    # Step 1: Compute the (encoding) distance between the anchor and the positive
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,positive)),axis=-1)
+    # Step 2: Compute the (encoding) distance between the anchor and the negative
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,negative)),axis=-1)
+    # Step 3: subtract the two previous distances and add alpha.
+    basic_loss = tf.maximum(tf.add(tf.subtract(pos_dist,neg_dist),alpha),0)
+    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
+    loss = tf.reduce_sum(basic_loss)
+    
+    return loss
+
+#tf.keras.backend.set_image_data_format('channels_last')
+def img_to_encoding(image_path, model):
+    """Runs the model's forward propagation on the specified image."""
+    img = tf.keras.preprocessing.image.load_img(image_path, target_size=(160, 160))
+    img = np.around(np.array(img) / 255.0, decimals=12)
+    x_train = np.expand_dims(img, axis=0)
+    embedding = model.predict_on_batch(x_train)
+    return embedding / np.linalg.norm(embedding, ord=2)
+
+def verify(image_path, identity, database, model):
+    """
+    Function that verifies if the person on the "image_path" image is "identity".
+    
+    Arguments:
+        image_path -- path to an image
+        identity -- string, name of the person you'd like to verify the identity. Has to be an employee who works in the office.
+        database -- python dictionary mapping names of allowed people's names (strings) to their encodings (vectors).
+        model -- your Inception model instance in Keras
+    
+    Returns:
+        dist -- distance between the image_path and the image of "identity" in the database.
+        door_open -- True, if the door should open. False otherwise.
+    """
+    # Step 1: Compute the encoding for the image. Use img_to_encoding() see example above.
+    encoding = img_to_encoding(image_path,model)
+    # Step 2: Compute distance with identity's image
+    dist = np.linalg.norm(encoding - database[identity])
+    # Step 3: Open the door if dist < 0.7, else don't open
+    if dist < 0.7:
+        print("It's " + str(identity) + ", welcome in!")
+        door_open = True
+    else:
+        print("It's not " + str(identity) + ", please go away")
+        door_open = False     
+    return dist, door_open
+
+def who_is_it(image_path, database, model):
+    """
+    Implements face recognition for the office by finding who is the person on the image_path image.
+    
+    Arguments:
+        image_path -- path to an image
+        database -- database containing image encodings along with the name of the person on the image
+        model -- your Inception model instance in Keras
+    
+    Returns:
+        min_dist -- the minimum distance between image_path encoding and the encodings from the database
+        identity -- string, the name prediction for the person on image_path
+    """
+
+    ## Step 1: Compute the target "encoding" for the image. Use img_to_encoding() see example above. 
+    encoding = img_to_encoding(image_path,model)
+    
+    ## Step 2: Find the closest encoding ##
+    
+    # Initialize "min_dist" to a large value, say 100
+    min_dist = 100
+    
+    # Loop over the database dictionary's names and encodings.
+    for (name, db_enc) in database.items():
+        
+        # Compute L2 distance between the target "encoding" and the current db_enc from the database. 
+        dist = np.linalg.norm(encoding - db_enc)
+
+        # If this distance is less than the min_dist, then set min_dist to dist, and identity to name. 
+        if dist < min_dist:
+            min_dist = dist
+            identity = name
+    
+    if min_dist > 0.7:
+        print("Not in the database.")
+    else:
+        print ("it's " + str(identity) + ", the distance is " + str(min_dist))
+        
+    return min_dist, identity
---
+++ 16. Art Generation with Neural Style Transfer/art_generation.py
@@ -0,0 +1,215 @@
+import os
+import sys
+import scipy.io
+import scipy.misc
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+from public_tests import *
+
+STYLE_LAYERS = [
+    ('block1_conv1', 0.2),
+    ('block2_conv1', 0.2),
+    ('block3_conv1', 0.2),
+    ('block4_conv1', 0.2),
+    ('block5_conv1', 0.2)]
+
+content_layer = [('block5_conv4', 1)]
+
+def compute_content_cost(content_output, generated_output):
+    """
+    Computes the content cost
+    
+    Arguments:
+    a_C -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image C 
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image G
+    
+    Returns: 
+    J_content -- scalar that you compute using equation 1 above.
+    """
+    a_C = content_output[-1]
+    a_G = generated_output[-1]
+    
+    # Retrieve dimensions from a_G
+    _, n_H, n_W, n_C = a_G.get_shape().as_list()
+    
+    # Reshape 'a_C' and 'a_G'
+    # DO NOT reshape 'content_output' or 'generated_output'
+    a_C_unrolled = tf.reshape(a_C, shape=[1, n_H * n_W, n_C])
+    a_G_unrolled = tf.reshape(a_G, shape=[1, n_H * n_W, n_C])
+    
+    # compute the cost with tensorflow
+    J_content = tf.reduce_sum(tf.square(a_C_unrolled - a_G_unrolled)) / (4.0 * n_H * n_W * n_C)
+    
+    return J_content
+
+def gram_matrix(A):
+    """
+    Argument:
+    A -- matrix of shape (n_C, n_H*n_W)
+    
+    Returns:
+    GA -- Gram matrix of A, of shape (n_C, n_C)
+    """ 
+    # [matmul](https://www.tensorflow.org/api_docs/python/tf/matmul) and [transpose](https://www.tensorflow.org/api_docs/python/tf/transpose).
+    GA = tf.matmul(A, tf.transpose(A))
+
+    return GA
+
+def compute_layer_style_cost(a_S, a_G):
+    """
+    Arguments:
+    a_S -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image S 
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image G
+    
+    Returns: 
+    J_style_layer -- tensor representing a scalar value, style cost defined above by equation (2)
+    """
+    # Retrieve dimensions from a_G
+    _, n_H, n_W, n_C = a_G.get_shape().as_list()
+    
+    # Reshape the tensors from (1, n_H, n_W, n_C) to (n_C, n_H * n_W) 
+    a_S = tf.transpose(tf.reshape(a_S, shape=[-1, n_C]))
+    a_G = tf.transpose(tf.reshape(a_G, shape=[-1, n_C]))
+
+    # Computing gram_matrices for both images S and G 
+    GS = gram_matrix(a_S)
+    GG = gram_matrix(a_G)
+
+    # Computing the loss
+    J_style_layer = tf.reduce_sum(tf.square(GS - GG)) / (4.0 *(( n_H * n_W * n_C)**2))
+        
+    return J_style_layer
+
+def compute_style_cost(style_image_output, generated_image_output, STYLE_LAYERS=STYLE_LAYERS):
+    """
+    Computes the overall style cost from several chosen layers
+    
+    Arguments:
+    style_image_output -- our tensorflow model
+    generated_image_output --
+    STYLE_LAYERS -- A python list containing:
+                        - the names of the layers we would like to extract style from
+                        - a coefficient for each of them
+    
+    Returns: 
+    J_style -- tensor representing a scalar value, style cost defined above by equation (2)
+    """
+    
+    # initialize the overall style cost
+    J_style = 0
+
+    # Set a_S to be the hidden layer activation from the layer we have selected.
+    # The last element of the array contains the content layer image, which must not be used.
+    a_S = style_image_output[:-1]
+
+    # Set a_G to be the output of the choosen hidden layers.
+    # The last element of the list contains the content layer image which must not be used.
+    a_G = generated_image_output[:-1]
+    for i, weight in zip(range(len(a_S)), STYLE_LAYERS):  
+        # Compute style_cost for the current layer
+        J_style_layer = compute_layer_style_cost(a_S[i], a_G[i])
+
+        # Add weight * J_style_layer of this layer to overall style cost
+        J_style += weight[1] * J_style_layer
+
+    return J_style
+
+def clip_0_1(image):
+    """
+    Truncate all the pixels in the tensor to be between 0 and 1
+    
+    Arguments:
+    image -- Tensor
+    J_style -- style cost coded above
+
+    Returns:
+    Tensor
+    """
+    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+def tensor_to_image(tensor):
+    """
+    Converts the given tensor into a PIL image
+    
+    Arguments:
+    tensor -- Tensor
+    
+    Returns:
+    Image: A PIL image
+    """
+    tensor = tensor * 255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor) > 3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return Image.fromarray(tensor)
+
+def get_layer_outputs(vgg, layer_names):
+    """ Creates a vgg model that returns a list of intermediate output values."""
+    outputs = [vgg.get_layer(layer[0]).output for layer in layer_names]
+
+    model = tf.keras.Model([vgg.input], outputs)
+    return model
+
+@tf.function()
+def total_cost(J_content, J_style, alpha = 10, beta = 40):
+    """
+    Computes the total cost function
+    
+    Arguments:
+    J_content -- content cost coded above
+    J_style -- style cost coded above
+    alpha -- hyperparameter weighting the importance of the content cost
+    beta -- hyperparameter weighting the importance of the style cost
+    
+    Returns:
+    J -- total cost as defined by the formula above.
+    """
+
+    J = alpha * J_content + beta * J_style
+
+    return J
+
+optimizer_adam = tf.keras.optimizers.Adam(learning_rate=0.01)
+
+@tf.function()
+def train_step(generated_image, optimizer=optimizer_adam, vgg=None, style_layers=STYLE_LAYERS, content_layer=content_layer):
+    with tf.GradientTape() as tape:
+        # In this function you must use the precomputed encoded images a_S and a_C
+        vgg_model_outputs = get_layer_outputs(vgg, style_layers + content_layer)
+
+        # Compute a_G as the vgg_model_outputs for the current generated image
+        a_G = vgg_model_outputs(generated_image)
+        
+        # Compute the style cost
+        J_style = compute_style_cost(a_S, a_G)
+
+        # Compute the content cost
+        J_content = compute_content_cost(a_C, a_G)
+        # Compute the total cost
+        J = total_cost(J_content, J_style,alpha = 10, beta = 40)
+        
+    grad = tape.gradient(J, generated_image)
+
+    optimizer.apply_gradients([(grad, generated_image)])
+    generated_image.assign(clip_0_1(generated_image))
+    # For grading purposes
+    return J
+
+
+# Usage:
+# Show the generated image at some epochs
+# Uncomment to reset the style transfer process. You will need to compile the train_step function again 
+# epochs = 2501
+# for i in range(epochs):
+#     train_step(generated_image)
+#     if i % 250 == 0:
+#         print(f"Epoch {i} ")
+#     if i % 250 == 0:
+#         image = tensor_to_image(generated_image)
+#         imshow(image)
+#         image.save(f"output/image_{i}.jpg")
+#         plt.show() 
