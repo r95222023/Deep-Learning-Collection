8f1704c6dd1e2315ee7a885781f17685ed157458
Bo-Yan Huang <r95222023@gmail.com>
2024-08-25 13:25:39 +0800

    updated readme

--- 3. Building Deep Neural Network/deep_neural_network_utils.py
+++ 3. Building Deep Neural Network/deep_neural_network_utils.py
@@ -1,10 +1,5 @@
 import numpy as np
-import h5py
-import matplotlib.pyplot as plt
-from testCases import *
 from dnn_utils import sigmoid, sigmoid_backward, relu, relu_backward
-from public_tests import *
-
 import copy
 
 def initialize_parameters(n_x, n_h, n_y):
